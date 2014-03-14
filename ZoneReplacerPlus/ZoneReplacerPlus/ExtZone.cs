b0ed806b1f93c9ceeb9ba13d97a47bbb90b86af6
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-14 11:39:09 -0400

    Initial Project Create

---
+++ ZoneReplacerPlus/ZoneReplacerPlus/ExtZone.cs
@@ -0,0 +1,50 @@
+﻿namespace ZoneReplacer
+{
+    internal class ExtZone : Zone
+    {
+        private string _click;
+        private string _doubleClick;
+        private string _extField;
+
+        private string _rightClick;
+
+        private string _type;
+
+        public ExtZone(string[] receivers, string top, string left, string height, string width, string columnCount, string extField, string type)
+            : base(receivers, top, left, height, width, columnCount)
+        {
+            ExtField = extField;
+            Type = type;
+        }
+
+        public string Click
+        {
+            get { return _click; }
+            set { _click = value; }
+        }
+
+        public string DoubleClick
+        {
+            get { return _doubleClick; }
+            set { _doubleClick = value; }
+        }
+
+        public string ExtField
+        {
+            get { return _extField; }
+            set { _extField = value; }
+        }
+
+        public string RightClick
+        {
+            get { return _rightClick; }
+            set { _rightClick = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+    }
+}
---
+++ ZoneReplacerPlus/ZoneReplacerPlus/Zone.cs
@@ -0,0 +1,74 @@
+﻿namespace ZoneReplacer
+{
+    internal class Zone
+    {
+        private string _borderStyle;
+        private string _columnCount;
+        private string _height;
+        private string _left;
+        private string[] _receivers;
+
+        private string _top;
+
+        private string _width;
+
+        public Zone(string[] receivers, string top, string left, string height, string width, string columnCount)
+        {
+            Receivers = receivers;
+            Top = top;
+            Left = left;
+            Height = height;
+            Width = width;
+            ColumnCount = columnCount;
+            BorderStyle = "0";
+        }
+
+        public Zone(string[] receivers, string top, string left, string height, string width, string columnCount, string borderStyle)
+            : this(receivers, top, left, height, width, columnCount)
+        {
+            BorderStyle = borderStyle;
+        }
+
+        public string BorderStyle
+        {
+            get { return _borderStyle; }
+            set { _borderStyle = value; }
+        }
+
+        public string ColumnCount
+        {
+            get { return _columnCount; }
+            set { _columnCount = value; }
+        }
+
+        public string Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public string Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+
+        public string[] Receivers
+        {
+            get { return _receivers; }
+            set { _receivers = value; }
+        }
+
+        public string Top
+        {
+            get { return _top; }
+            set { _top = value; }
+        }
+
+        public string Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+    }
+}
