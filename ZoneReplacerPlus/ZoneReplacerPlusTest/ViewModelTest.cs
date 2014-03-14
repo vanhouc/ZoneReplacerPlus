2d3b030f0d233f5755cb34952194179d87525c9b
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-14 12:14:08 -0400

    Added Test Project

---
+++ ZoneReplacerPlus/ZoneReplacerPlusTest/ViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZoneReplacerPlusTest
+{
+    [TestClass]
+    public class ViewModelTest
+    {
+        [TestMethod]
+        public void LoadZones()
+        {
+        }
+    }
+}
